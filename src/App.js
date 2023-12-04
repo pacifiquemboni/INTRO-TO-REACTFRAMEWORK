f9e6f9d75bf62de5ed00d5c97a9ea35dd1a4ff76
G O A T PC <pacifiquemboni>
2023-12-04 20:37:08 +0200

    working with JSX Component and Props

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@
 import './App.css';
 
 function App() {
+ 
   return (
     <div className="App">
-      <h1>PACIFIQUE</h1>
+     <Job salary={90000} position= "Senior SDE" company="Amazon"/>
+     <Job salary={12000} position= "Junior SDE" company="Googlr"/>
+     <Job salary={10000} position= "Project Manager" company="Netflix"/>
     </div>
   );
 }
+const Job = (props) => {
+  return (
+    <div>
+      <h1>{props.salary}</h1>
+     
+      <h1>{props.position}</h1>
+     
+      <h1>{props.company}</h1> <hr></hr>
+    </div>
+  );
+};
+
 
 export default App;
